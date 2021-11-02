673fca7a2b1698015945e0838782d58e02676e37
DavidLindtner <lindtnerdavid@gmail.com>
2021-11-02 22:19:59 +0100

    edit formatting of file names

--- TrailCamera.cpp
+++ TrailCamera.cpp
@@ -39,7 +39,7 @@ std::string CurrentTime(void)
 	char buff [30];
 	time_t t = time(NULL);
 	struct tm curr_time = *localtime(&t);
-	sprintf(buff, "%d_%d_%d_%d_%d_%d", curr_time.tm_year+1900, curr_time.tm_mon+1, curr_time.tm_mday, curr_time.tm_hour, curr_time.tm_min, curr_time.tm_sec);
+	sprintf(buff, "%d_%02d_%02d_%02d_%02d_%02d", curr_time.tm_year+1900, curr_time.tm_mon+1, curr_time.tm_mday, curr_time.tm_hour, curr_time.tm_min, curr_time.tm_sec);
 	std::string retVal(buff);
 	return (retVal);
 }
